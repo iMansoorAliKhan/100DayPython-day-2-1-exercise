cb0d925e1c84b4827515393c9dc101779924a43b
Mansoor Ali Khan <connect@mansooralikhan.com>
2020-12-13 18:41:27 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+# 🚨 Don't change the code below 👇
+two_digit_number = input("Type a two digit number: ")
+# 🚨 Don't change the code above 👆
+
+####################################
+#Write your code below this line 👇
+
+dig1 = two_digit_number[0]
+dig2 = two_digit_number[1]
+sum = int(dig1)+int(dig2)
+print("The sum of 2 digits ", dig1 + " and ",dig2+" is " +str(sum))
